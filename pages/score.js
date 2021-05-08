7f4606f9135e9c577f45113b4ef3aace569f603a
Christian Montes <christian.montes8991@gmail.com>
2021-05-08 02:49:22 -0700

    First draft flexbox layout

---
+++ pages/score.js
@@ -0,0 +1,14 @@
+import Layout from '../components/layout';
+import Score from '../components/score';
+
+export default function ScorePage() {
+  return (
+
+    <Layout>
+      <Score />
+      <Score />
+      <Score />
+    </Layout>
+    
+  )
+}
