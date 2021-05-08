ff6656b74058d769bc6c37c02bd309fb47010897
Christian Montes <christian.montes8991@gmail.com>
2021-05-08 02:48:01 -0700

    Minor change css -> scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 // importing bootstrap stylesheet
 import 'bootstrap/dist/css/bootstrap.css';
 
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
