5ef8cebaf881e87022566447f9ba17fe337483ac
Christian Montes <christian.montes8991@gmail.com>
2021-05-12 03:10:10 -0700

    Extracted parts to Team component; DRY code

---
+++ components/team.js
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBaseballBall } from '@fortawesome/free-solid-svg-icons';
+
+export default function Team({ away, teamData, runsScored }) {
+  const {
+    teamName,
+    record: { wins, losses },
+  } = teamData;
+
+  return (
+    <>
+      <div
+        className="row h-50 align-items-center"
+        style={
+          away
+            ? { paddingBottom: '0px', marginBottom: '0px' }
+            : { paddingTop: '0px', marginTop: '0px' }
+        }
+      >
+        <div className="col-2 px-1">
+          <span style={{ fontSize: '23px' }}>
+            <FontAwesomeIcon icon={faBaseballBall} />
+          </span>
+        </div>
+        <div className="col-8 h-100 ps-2.5">
+          <div
+            className="row h-50"
+            style={{ fontSize: '13px', paddingBottom: '0px' }}
+          >
+            {teamName}
+            {/* Name */}
+          </div>
+          <div
+            className="row h-50"
+            style={{ fontSize: '10px', marginBottom: '0px' }}
+          >
+            {/* 18-17 */}
+            {wins}-{losses}
+          </div>
+        </div>
+        <div className="col-2 ps-0">{runsScored}</div>
+      </div>
+    </>
+  );
+}
