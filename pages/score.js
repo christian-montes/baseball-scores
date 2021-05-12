c3fb72348c45de5ce328fb2c8da7022327669f01
Christian Montes <christian.montes8991@gmail.com>
2021-05-12 03:06:40 -0700

    Added dynamic rendering of Score components

--- pages/score.js
+++ pages/score.js
@@ -1,14 +1,42 @@
 import Layout from '../components/layout';
 import Score from '../components/score';
 
-export default function ScorePage() {
+import axios from 'axios'
+// import useSWR from 'swr';
+
+export default function ScorePage({ todaysGames }) {
+  // destructuring data object to get date and games
+  const { dates: [{ date, games }]} = todaysGames;
+  const GameComponents = games.map(
+    game => {
+      return (
+        <Score key={game.gamePk} link={game.link} />
+      )
+    }
+  )
+
+  // console.log(todaysGames);
+
   return (
 
-    <Layout>
-      <Score />
-      <Score />
-      <Score />
+    <Layout date={date}>
+      {GameComponents}
     </Layout>
     
   )
+}
+
+export async function getServerSideProps(context) {
+
+  const todaysGames = await axios.get(
+    'https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1'
+  ).then(res => res.data);
+
+  if (!todaysGames) {
+    return {
+      notFound: true
+    }
+  }
+
+  return { props: { todaysGames } }
 }
