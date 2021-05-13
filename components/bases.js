a8e3be06c53c6bd2647c7cae40d97b5d5eb90dd4
Christian Montes <christian.montes8991@gmail.com>
2021-05-13 02:57:58 -0700

    Added ball-strike count and outs tracker

--- components/bases.js
+++ components/bases.js
@@ -1,27 +1,44 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareFull } from '@fortawesome/free-solid-svg-icons';
-import styles from './bases.module.scss'
+import { useState } from 'react';
+import styles from './bases.module.scss';
 
-export default function Bases() {
+export default function Bases({ gameState, count }) {
+  // const [vizSetting, setVizSetting] = useState({ visibility: 'visible' });
+  console.log(gameState);
+  const { balls, strikes, outs } = count;
+
+  // if (gameState === 'Final' || 'Preview') {
+  //   return (
+  //     <>
+  //       <div className={styles.field}></div>
+  //     </>
+  //   );
+  // }
+
   return (
     <>
       <div className={styles.field}>
+        {(gameState === 'In Progress' || gameState === 'Warmup') && (
+          <>
+            <span className={styles.firstBase} id="first-base">
+              <FontAwesomeIcon icon={faSquareFull} />
+            </span>
 
-        <span className={styles.firstBase} id='first-base'>
-          <FontAwesomeIcon icon={faSquareFull} />
-        </span>
+            <span className={styles.secondBase} id="second-base">
+              <FontAwesomeIcon icon={faSquareFull} />
+            </span>
 
-        <span className={styles.secondBase} id='second-base'>
-          <FontAwesomeIcon icon={faSquareFull} />
-        </span>
+            <span className={styles.thirdBase} id="second-base">
+              <FontAwesomeIcon icon={faSquareFull} />
+            </span>
 
-        <span className={styles.thirdBase} id='second-base'>
-          <FontAwesomeIcon icon={faSquareFull} />
-        </span>
-
-        <span className={styles.count} id='count'>0-2, 2 out</span>
-
+            <span className={styles.count} id="count">
+              {balls}-{strikes}, {outs} out
+            </span>
+          </>
+        )}
       </div>
     </>
-  )
+  );
 }
