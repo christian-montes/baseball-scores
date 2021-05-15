ad17e82ad103b8e02c0193096028973e408b25c7
Christian Montes <christian.montes8991@gmail.com>
2021-05-15 01:30:19 -0700

    Mapping out baserunners logic

--- components/bases.js
+++ components/bases.js
@@ -1,37 +1,34 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSquareFull } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import styles from './bases.module.scss';
 
-export default function Bases({ gameState, count }) {
-  // const [vizSetting, setVizSetting] = useState({ visibility: 'visible' });
-  console.log(gameState);
+export default function Bases({ gameState, count, plays }) {
+  const [outnum, setOut] = useState(null);
   const { balls, strikes, outs } = count;
+  const display = ['In Progress', 'Warmup'].indexOf(gameState) >= 0;
 
-  // if (gameState === 'Final' || 'Preview') {
-  //   return (
-  //     <>
-  //       <div className={styles.field}></div>
-  //     </>
-  //   );
-  // }
+  if (outnum === null) setOut(outs);
+  /**
+   * If the length of the plays array is one,
+   * that play will set the state of runners.
+   * 
+   * if the length of the array is gt one, 
+   * the previous play will set the state of runners,
+   * UNLESS the previous play made the third out
+   */
+  const first = null;
+  const second = null;
+  const third = null;
 
   return (
     <>
       <div className={styles.field}>
-        {(gameState === 'In Progress' || gameState === 'Warmup') && (
+        {display && (
           <>
-            <span className={styles.firstBase} id="first-base">
-              <FontAwesomeIcon icon={faSquareFull} />
-            </span>
+            <span className={styles.firstBase} id="first-base"></span>
 
-            <span className={styles.secondBase} id="second-base">
-              <FontAwesomeIcon icon={faSquareFull} />
-            </span>
+            <span className={styles.secondBase} id="second-base"></span>
 
-            <span className={styles.thirdBase} id="second-base">
-              <FontAwesomeIcon icon={faSquareFull} />
-            </span>
+            <span className={styles.thirdBase} id="second-base"></span>
 
             <span className={styles.count} id="count">
               {balls}-{strikes}, {outs} out
