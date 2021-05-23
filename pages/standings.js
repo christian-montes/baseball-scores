61352825c36c6971644af9499e5ac416530888d2
Christian Montes <christian.montes8991@gmail.com>
2021-05-23 01:03:46 -0700

    Standings page; fetches data from ESPN API

---
+++ pages/standings.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import Layout from '../components/layout';
+import Standings from '../components/standings';
+
+export async function getStaticProps() {
+  const { children } = await axios
+    .get('https://site.api.espn.com/apis/v2/sports/baseball/mlb/standings')
+    .then((res) => res.data);
+
+  const { groups } = await axios
+    .get(
+      'https://site.web.api.espn.com/apis/site/v2/sports/baseball/mlb/groups'
+    )
+    .then((res) => res.data);
+
+  const teamDivisions = {};
+
+  groups.map((league) => {
+    const { children } = league;
+
+    children.map((division) => {
+      const { name: fullDivisionName, teams } = division;
+      const region = fullDivisionName.split(' ')[2];
+      // console.log(region);
+      teams.map((team) => {
+        const { abbreviation } = team;
+        teamDivisions[abbreviation] = region;
+      });
+    });
+  });
+
+  // console.log(teamDivisions);
+
+  children.map((league) => {
+    const {
+      standings: { entries },
+    } = league;
+    entries.map((entry) => {
+      const {
+        team: { abbreviation },
+      } = entry;
+      entry['team']['division'] = teamDivisions[abbreviation];
+    });
+  });
+
+  const [AmericanLeague, NationalLeague] = children;
+
+  if (!AmericanLeague || !NationalLeague) {
+    return { notFound: true };
+  }
+
+  return { props: { AmericanLeague, NationalLeague } };
+}
+
+export default function StandingsPage({ AmericanLeague, NationalLeague }) {
+  // console.log(AmericanLeague)
+  const dateProp = new Date();
+  return (
+    <Layout date={dateProp} >
+      <Standings data={AmericanLeague} />
+    </Layout>
+  );
+}
