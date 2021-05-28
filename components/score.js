5266598fd89167e0f379d2be26c53e742b7e6a39
Christian Montes <christian.montes8991@gmail.com>
2021-05-28 02:23:41 -0700

    Added new view for score component onClick

--- components/score.js
+++ components/score.js
@@ -5,12 +5,15 @@ import SkeletonScore from './skeletons/SkeletonScore';
 
 import axios from 'axios';
 import useSWR from 'swr';
+import Decision from './gameDecisions';
+import { useState } from 'react';
 
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 export default function Score({ link }) {
   const gameURL = `https://statsapi.mlb.com${link}`;
   const { data } = useSWR(gameURL, fetcher, { refreshInterval: 10000 });
+  const [viewDecisions, setViewDecisions] = useState(false);
   // console.log(data);
 
   // extracting the necessary data to passdown to Team component;
@@ -33,48 +36,75 @@ export default function Score({ link }) {
         outs,
       },
       plays: { allPlays, playsByInning },
+      decisions,
     },
   } = data;
   // console.log(detailedState);
 
-  return (
-    <div className="score container py-1">
-      <div className="row" style={{ height: '4.5rem' }}>
-        <div className="col-7 col-sm-6">
-          <Team
-            away
-            teamRecord={awayRecord}
-            teamRunData={teamRuns}
-            gameState={detailedState}
-          />
-          <Team
-            teamRecord={homeRecord}
-            teamRunData={teamRuns}
-            gameState={detailedState}
-          />
-        </div>
+  const toggleViewDecisions = (event) => {
+    event.preventDefault();
 
-        <div className="col pe-0 h-100 position-relative">
-          <div className="position-absolute top-50 end-50 translate-middle pe-0">
-            <Bases
-              gameState={detailedState}
-              count={{ balls, strikes, outs, inningNumber }}
-              plays={{ allPlays, playsByInning, inningHalf }}
-            />
-          </div>
-          <Inning
-            gameState={detailedState}
-            inningData={{
-              inningNumber,
-              nth_Inning,
-              inningHalf,
-              scheduledInnings,
-            }}
-            outs={outs}
-            teamRunData={teamRuns}
-            timeData={datetime}
+    if (detailedState === 'Final') {
+      setViewDecisions(!viewDecisions);
+    }
+  };
+
+  return (
+    <div
+      className="score container py-1"
+      style={{ backgroundColor: '#405259' }}
+    >
+      <div
+        className="row"
+        style={{ height: '4.5rem' }}
+        onClick={toggleViewDecisions}
+      >
+        {viewDecisions ? (
+          <Decision
+            teamRuns={teamRuns}
+            teamInfoAway={awayRecord}
+            teamInfoHome={homeRecord}
+            decisions={decisions}
           />
-        </div>
+        ) : (
+          <>
+            <div className="col-7 col-sm-6">
+              <Team
+                away
+                teamRecord={awayRecord}
+                teamRunData={teamRuns}
+                gameState={detailedState}
+              />
+              <Team
+                teamRecord={homeRecord}
+                teamRunData={teamRuns}
+                gameState={detailedState}
+              />
+            </div>
+
+            <div className="col pe-0 h-100 position-relative">
+              <div className="position-absolute top-50 end-50 translate-middle pe-0">
+                <Bases
+                  gameState={detailedState}
+                  count={{ balls, strikes, outs, inningNumber }}
+                  plays={{ allPlays, playsByInning, inningHalf }}
+                />
+              </div>
+              <Inning
+                gameState={detailedState}
+                inningData={{
+                  inningNumber,
+                  nth_Inning,
+                  inningHalf,
+                  scheduledInnings,
+                }}
+                outs={outs}
+                teamRunData={teamRuns}
+                timeData={datetime}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
