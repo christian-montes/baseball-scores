79e16b3fa4bc7d8824636bba20f9e2b1bbef0488
Christian Montes <christian.montes8991@gmail.com>
2021-05-31 00:11:54 -0700

    Added footer links and new styling

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,46 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './layout.module.scss';
 import moment from 'moment';
 
 export default function Layout({ date, children, page }) {
   const formattedDate = moment(date).format('dddd MMM D');
+
+  const footerPaths = [
+    { name: 'Home', link: '/' },
+    { name: 'Scores', link: '/score' },
+    { name: 'Standings', link: '/standings' },
+    {
+      name: 'About',
+      link: 'https://github.com/christian-montes/baseball-scores',
+    },
+  ];
+
+  const footerLinks = footerPaths.map((path) => {
+    return (
+      <li key={path.name}>
+        <Link href={path.link}>{path.name}</Link>
+      </li>
+    );
+  });
   return (
     <>
-      {/* <Head>
-        <meta name='keywords' content='MLB baseball scores live' />
-        <meta name='og:title' content='Live MLB Scores' />
-        <meta name='twitter:card' content='Live Baseball score feed' />
-      </Head> */}
+      {page === 'index' ? (
+        <Head>
+          <title>Daily Baseball Scores and Standings</title>
+          <meta name="description" content="MLB Score Tracking" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="keywords" content="MLB baseball scores live" />
+          <meta name="og:title" content="Live MLB Scores" />
+        </Head>
+      ) : (
+        <Head>
+          <meta name="keywords" content="MLB baseball scores live" />
+          <meta name="og:title" content="Live MLB Scores" />
+          <meta name="twitter:card" content="Live Baseball score feed" />
+        </Head>
+      )}
 
       <header className={styles.parent}>
         <div className={styles.child}>
@@ -28,7 +57,15 @@ export default function Layout({ date, children, page }) {
           </div>
         )}
         <div className={styles.child}>
-          <div style={{ height: '60px', width: '60px' }} />
+          {page === 'index' ? (
+            <div style={{ width: '60px', height: '60px' }} />
+          ) : (
+            <div className={styles.menu}>
+              <div id="bar1" className={styles.bar} />
+              <div id="bar2" className={styles.bar} />
+              <div id="bar3" className={styles.bar} />
+            </div>
+          )}
         </div>
       </header>
 
@@ -41,13 +78,14 @@ export default function Layout({ date, children, page }) {
       )}
 
       <footer>
-        <a
+        <ul>{footerLinks}</ul>
+        {/* <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by NextJS
-        </a>
+        </a> */}
       </footer>
     </>
   );
