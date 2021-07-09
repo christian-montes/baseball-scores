7e36e016369cad7adbc14c217b9fff7ce6ab61fd
Christian Montes <christian.montes8991@gmail.com>
2021-07-09 02:10:28 -0700

    Component only visible in larger viewports

--- components/gameDecisions.js
+++ components/gameDecisions.js
@@ -1,42 +1,66 @@
-import styles from './gameDecisions.module.scss';
+import styles from '../styles/score.module.scss';
 
-export default function Decision({
-  teamRuns,
-  teamInfoAway,
-  teamInfoHome,
-  decisions,
-}) {
-  const { abbreviation: awayAbbr } = teamInfoAway;
-
-  const { abbreviation: homeAbbr } = teamInfoHome;
-
-  const {
-    home: { runs: runsHome },
-    away: { runs: runsAway },
-  } = teamRuns;
-
+export default function Decision({ decisions }) {
   const {
     winner: { fullName: nameWinner },
     loser: { fullName: nameLoser },
   } = decisions;
 
-  const lastNameWinner = nameWinner.split(' ').slice(-1);
-  const lastNameLoser = nameLoser.split(' ').slice(-1);
-  let lastNameSave = '';
+  // const lastNameWinner = nameWinner.split(' ').slice(-1);
+  // const lastNameLoser = nameLoser.split(' ').slice(-1);
+  let nameSave = 'N/A';
 
   if (decisions?.save) {
-    lastNameSave = decisions.save.fullName.split(' ').slice(-1);
+    nameSave = decisions.save.fullName;
   }
+
+  const labelStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    fontSize: '12px',
+    fontWeight: '300',
+  };
+
+  const dataStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    fontSize: '12px',
+    fontWeight: 'bold',
+  };
+
+  const noSaveLabel = {
+    ...labelStyle,
+    visibility: 'hidden',
+  };
+
+  const noSaveData = {
+    ...dataStyle,
+    visibility: 'hidden',
+  };
   return (
     <>
-      <div className={styles.final}>FINAL</div>
-      <div
-        className={styles.scores}
-      >{`${awayAbbr} ${runsAway}, ${homeAbbr} ${runsHome}`}</div>
-      <div className={styles.decisions}>
-        {lastNameSave
-          ? `W: ${lastNameWinner} L: ${lastNameLoser} S: ${lastNameSave}`
-          : `W: ${lastNameWinner} L: ${lastNameLoser}`}
+      <div className={styles.gameDecision}>
+        <div>
+          <div style={labelStyle}>WIN</div>
+          <div style={dataStyle}>{nameWinner}</div>
+        </div>
+        <div>
+          <div style={labelStyle}>LOSS</div>
+          <div style={dataStyle}>{nameLoser}</div>
+        </div>
+        <div>
+          <div style={nameSave === 'N/A' ? noSaveLabel : labelStyle}>SAVE</div>
+          <div style={nameSave === 'N/A' ? noSaveData : dataStyle}>
+            {nameSave}
+          </div>
+        </div>
+        {/* <div style={{ fontSize: '14px' }}>FINAL</div>
+        <div>{`${awayAbbr} ${runsAway}, ${homeAbbr} ${runsHome}`}</div>
+        <div style={{ fontSize: '14px', color: '#99A6AB' }}>
+          {lastNameSave
+            ? `W: ${lastNameWinner} L: ${lastNameLoser} S: ${lastNameSave}`
+            : `W: ${lastNameWinner} L: ${lastNameLoser}`}
+        </div> */}
       </div>
     </>
   );
