f296305c5f97cae3dc41073e9e4ad365b1672387
Christian Montes <christian.montes8991@gmail.com>
2021-07-09 02:17:06 -0700

    Individual game in season series

---
+++ components/seriesGame.js
@@ -0,0 +1,54 @@
+export default function SeriesGame({ gameData, gameNum }) {
+  const game = gameData['competitions'][0];
+  const [
+    {
+      team: { shortDisplayName: homeDisplayName },
+    },
+    {
+      team: { shortDisplayName: awayDisplayName },
+    },
+  ] = game['competitors'];
+
+  const homeScore = game['competitors'][0]?.score ? game['competitors'][0]['score']['displayValue'] : '-';
+  const awayScore = game['competitors'][1]?.score ? game['competitors'][1]['score']['displayValue'] : '-';
+
+  const teamRowStyle = { display: 'flex', justifyContent: 'space-around' };
+
+  return (
+    <>
+      <div style={{ height: '3rem', width: '100%', display: 'flex', marginBottom: '10px' }}>
+        <div
+          id="series-gamescore"
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-around',
+            flexGrow: '1',
+          }}
+        >
+          <div style={teamRowStyle}>
+            <div>{awayDisplayName}</div>
+            <div>{awayScore}</div>
+          </div>
+          <div style={teamRowStyle}>
+            <div>{homeDisplayName}</div>
+            <div>{homeScore}</div>
+          </div>
+        </div>
+        <div
+          id="gamedate"
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            flexGrow: '1',
+            alignItems: 'center'
+          }}
+        >
+          <div>{`Game ${gameNum}`}</div>
+          <div>7/24</div>
+        </div>
+      </div>
+    </>
+  );
+}
