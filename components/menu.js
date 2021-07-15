bf3eb8691ed83181073326d092824e4dcaa4b06c
Christian Montes <christian.montes8991@gmail.com>
2021-07-15 02:39:03 -0700

    New Menu look

--- components/menu.js
+++ components/menu.js
@@ -1,27 +1,30 @@
 import styles from './menu.module.scss';
 import Link from 'next/link';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Menu({ show, clickCallback }) {
-  // const [elemHeight, setElemHeight] = useState('0');
-  // const [windowWidth, setWindowWidth] = useState(0);
+  const [page, setPage] = useState('');
   const menuLinks = [
     { name: 'Home', link: '/' },
     { name: 'Scores', link: '/scores' },
     { name: 'Standings', link: '/standings' },
   ];
+
+  const currentPage = { backgroundColor: '#2F567D' };
+  const nonCurrentPage = { backgroundColor: 'inherit' };
   const menuItems = menuLinks
     .map((item) => {
       return (
         <li key={item.name}>
           <Link href={item.link}>
-            <a onClick={clickCallback}>{item.name}</a>
+            <a
+              style={page === item.link ? currentPage : nonCurrentPage}
+              onClick={clickCallback}
+            >
+              {item.name}
+            </a>
           </Link>
         </li>
-
-        // <a key={item.name} href={item.link} onClick={clickCallback}>
-        //   <li>{item.name}</li>
-        // </a>
       );
     })
     .concat(
@@ -37,40 +40,19 @@ export default function Menu({ show, clickCallback }) {
       </li>
     );
 
-  // function getHeight() {
-  //   var windowH = window.innerHeight;
-  //   var header = document.querySelector('header');
-  //   let headerHeight = header.offsetHeight;
-  //   headerHeight += parseInt(
-  //     window.getComputedStyle(header).getPropertyValue('margin-top')
-  //   );
-  //   headerHeight += parseInt(
-  //     window.getComputedStyle(header).getPropertyValue('margin-bottom')
-  //   );
+  function checkWindowLocation() {
+    setPage(window.location.pathname);
+  }
 
-  //   let menuHeight = String(windowH - headerHeight);
-  //   menuHeight += 'px';
-  //   if (window.innerWidth < 500) {
-  //     setElemHeight(menuHeight);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   getHeight();
-  // }, [windowWidth]);
-
-  // useEffect(() => {
-  //   var windowW = window.innerWidth;
-  //   console.log(windowW)
-  //   setWindowWidth(windowW);
-  // })
+  useEffect(() => {
+    checkWindowLocation();
+  }, []);
 
   return (
-    <div
-      id="menuContainer"
-      className={show ? styles.dropdownContent : styles.hiddenMenu}
-    >
-      <ul>{menuItems}</ul>
-    </div>
+    <>
+      <div className={show ? styles.menuContViz : styles.menuContHidden}>
+        <ul className={styles.menu}>{menuItems}</ul>
+      </div>
+    </>
   );
 }
