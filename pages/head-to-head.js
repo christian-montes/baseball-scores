31dda2b3772d592496f9443f5836d3662844e5bd
Christian Montes <christian.montes8991@gmail.com>
2022-06-09 01:10:13 -0700

    Added new page

---
+++ pages/head-to-head.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout';
+
+
+export default function Head2Head() {
+  return (
+    <Layout>
+      <main>
+        Page in development
+      </main>
+    </Layout>
+  )
+}
