e850295532fcefbe72597026d46aa6f90c2a1467
Christian Montes <christian.montes8991@gmail.com>
2022-06-27 00:08:48 -0700

    Changed basic structure of component

--- components/seriesGame.js
+++ components/seriesGame.js
@@ -1,4 +1,5 @@
 import styles from '../styles/seasonSeries.module.scss';
+import { format } from 'date-fns';
 
 export default function SeriesGame({ gameData, gameNum }) {
   const game = gameData['competitions'][0];
@@ -11,30 +12,31 @@ export default function SeriesGame({ gameData, gameNum }) {
     },
   ] = game['competitors'];
 
-  const homeScore = game['competitors'][0]?.score ? game['competitors'][0]['score']['displayValue'] : '-';
-  const awayScore = game['competitors'][1]?.score ? game['competitors'][1]['score']['displayValue'] : '-';
+  const homeScore = game['competitors'][0]?.score
+    ? game['competitors'][0]['score']['displayValue']
+    : '-';
+  const awayScore = game['competitors'][1]?.score
+    ? game['competitors'][1]['score']['displayValue']
+    : '-';
 
-  const teamRowStyle = { display: 'flex', justifyContent: 'space-around' };
+  const formattedGameDate = format(new Date(gameData.date), 'M/d');
+  // console.log(formattedGameDate);
 
   return (
     <>
       <div className={styles.entry}>
-        <div
-          id="series-gamescore"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            flexGrow: '1',
-          }}
-        >
-          <div style={teamRowStyle}>
+        <div id="series-gamescore" className={styles.teams}>
+          <div className={styles.teamRowStyle}>
             <div>{awayDisplayName}</div>
-            <div>{awayScore}</div>
+            <div style={{ width: '19px', height: '19px', textAlign: 'center' }}>
+              {awayScore}
+            </div>
           </div>
-          <div style={teamRowStyle}>
+          <div className={styles.teamRowStyle}>
             <div>{homeDisplayName}</div>
-            <div>{homeScore}</div>
+            <div style={{ width: '19px', height: '19px', textAlign: 'center' }}>
+              {homeScore}
+            </div>
           </div>
         </div>
         <div
@@ -44,11 +46,12 @@ export default function SeriesGame({ gameData, gameNum }) {
             flexDirection: 'column',
             justifyContent: 'center',
             flexGrow: '1',
-            alignItems: 'center'
+            width: '50%',
+            alignItems: 'center',
           }}
         >
           <div>{`Game ${gameNum}`}</div>
-          <div>7/24</div>
+          <div>{formattedGameDate}</div>
         </div>
       </div>
     </>
