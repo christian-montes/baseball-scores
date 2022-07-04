40325db6a508a22ee8735c8882eed976f80b8d34
Christian Montes <christian.montes8991@gmail.com>
2022-07-04 03:40:09 -0700

    name prop added

--- components/linescore.js
+++ components/linescore.js
@@ -1,6 +1,6 @@
 import styles from '../styles/linescore.module.scss';
 
-export default function LineScore({ linescoreData }) {
+export default function LineScore({ linescoreData, names }) {
   const {
     currentInning,
     scheduledInnings,
@@ -14,13 +14,21 @@ export default function LineScore({ linescoreData }) {
   // console.log(ArrayInnings);
 
   const topRow = ArrayInnings.map((inning) => {
-    return <div key={`Inning_${inning}`}>{inning}</div>;
+    return (
+      <div className="data" key={`Inning_${inning}`}>
+        {inning}
+      </div>
+    );
   });
 
   const awayInnings = inningsData.map((inning, index) => {
     const { away } = inning;
 
-    return <div key={`away_inning_${index}`}>{away.runs}</div>;
+    return (
+      <div className="data" key={`away_inning_${index}`}>
+        {away.runs}
+      </div>
+    );
   });
 
   const homeInnings = inningsData.map((inning, index) => {
@@ -31,15 +39,19 @@ export default function LineScore({ linescoreData }) {
   return (
     <div className={styles.threeSections}>
       <div className={styles.section1}>
-        <div className={styles.topRowElement}>Placehold</div>
-        <div>Team Away</div>
-        <div>Team Home</div>
+        {/* insert a placeholder here using css */}
+        <div className={styles.topRowElement}>&nbsp;</div>
+        <div>{names[0]}</div>
+        <div>{names[1]}</div>
       </div>
-      <div className={styles.section2}>
-        <div className={styles.topRowElement}>{topRow}</div>
-        <div>{awayInnings}</div>
-        <div>{homeInnings}</div>
+      <div id="linescore-wrapper" className={styles.linescore_wrapper}>
+        <div className={styles.section2}>
+          <div className={styles.topRowElement}>{topRow}</div>
+          <div>{awayInnings}</div>
+          <div>{homeInnings}</div>
+        </div>
       </div>
+
       <div className={styles.section3}>
         <div className={styles.topRowElement}>
           <div>R</div>
